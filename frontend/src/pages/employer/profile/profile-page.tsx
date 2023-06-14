fb7a460aed0c34e56cc0417d5a02907c9f5faa52
Wilmer Blanco <wilmer.blanco@weightwatchers.com>
2023-06-14 14:44:59 -0300

    Docker integration

---
+++ frontend/src/pages/employer/profile/profile-page.tsx
@@ -0,0 +1,13 @@
+import './profile-page.scss'
+import {Outlet} from "react-router-dom";
+
+function ProfilePage() {
+
+    return (
+        <>
+            <h1>Profile Page</h1>
+        </>
+    )
+}
+
+export default ProfilePage
