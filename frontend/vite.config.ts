79d73d9b8784bcd6aa2b62497bdb3c68a454d584
Wilmer Blanco <wilmervzla2000@gmail.com>
2023-10-25 18:41:54 -0300

    api app created

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
-    const API_BASE_URL = env.INTRANET_API_URL;
+    const API_BASE_URL = env.VITE_INTRANET_API_URL;
     console.log(API_BASE_URL);
     return {
         plugins: [react()],
